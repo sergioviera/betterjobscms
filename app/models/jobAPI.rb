c404a35eff64ceac81ea7e40d9cc69cb8c54b10f
Sergio Viera <sergioviera@gmail.com>
2013-04-27 15:44:42 -0300

    Setup heroku

---
+++ app/models/jobAPI.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'httparty'
+
+
+class JobAPI
+
+  include HTTParty
+
+#  format :xml
+
+  base_uri 'http://www.betterjobs.com/api/1' #'7e389902-c671-4248-b234-728021e753b4'
+#  base_uri 'http://dev.betterjobs.com:7777/OrionWeb/api/1' #/getJob?DeveloperKey=3a8b4813-2447-4d7b-a3c3-c30f66bac033&Keyword=java
+#  base_uri 'http://200.124.110.12:7777/OrionWeb/api/1'
+
+#http://www.betterjobs.com/api/1/getJob?DeveloperKey=7e389902-c671-4248-b234-728021e753b4&Keywords=java
+
+
+  def self.get_jobs(args)
+    params = {'DeveloperKey' => '7e389902-c671-4248-b234-728021e753b4',
+#    params = {'DeveloperKey' => '3a8b4813-2447-4d7b-a3c3-c30f66bac033',
+      'Keywords' => 'java',
+#      'Location' => l,
+      'CountryCode' => 'US'}
+
+    if !params[:k].nil?
+      if !params[:k].empty?
+        params.add( 'Keywords', params[:k] )
+      end
+    end
+
+    if !params[:l].nil?
+      if !params[:l].empty?
+        params.add( 'Location', params[:l] )
+      end
+    end
+
+#    puts get("http://www.betterjobs.com/api/1/getJob?DeveloperKey=7e389902-c671-4248-b234-728021e753b4&Keywords=ford&City=Dublin").inspect
+#    puts get("http://whoismyrepresentative.com/whoismyrep.php?zip=46544").inspect
+#    puts get('/getJob', :query => params).inspect
+
+#    @doc=Nokogiri::XML(open('http://www.betterjobs.com/api/1/getJob?DeveloperKey=7e389902-c671-4248-b234-728021e753b4&Keywords=java'),nil,"ISO-8859-1")
+
+    get('/getJob', :query => params)
+  end
+
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rickrockstar::Application.config.secret_token = '09f76c33e0f0e5f787841890da50437dc8022e17101c30d318b9a324103a046b51654dd2d6c0df59c6e20bd4b20879f2093ccad12537a0002676406a6ec14e45'
