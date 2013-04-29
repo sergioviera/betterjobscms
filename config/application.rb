850615f232193ace91d2e91a33f1bf9e81d7086b
Sergio Viera <sergioviera@gmail.com>
2013-04-29 10:09:44 -0300

    add initialize flag

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Rickrockstar
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # http://refinerycms.com/guides/heroku
+    config.assets.initialize_on_precompile = true
   end
 end
