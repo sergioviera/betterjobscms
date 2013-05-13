68e70e9d7d35d2fbae735b85643b04674c4b1192
Pravesh Ramachandran <pravesh.ramachandran@careerbuilder.com>
2013-05-13 16:54:43 -0500

    commit gemfile lock Try#2

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,11 +20,10 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 
-if ENV["development"]
-  ssh_options[:keys] = "/home/sergio/RoR/blogapp.pem"
-else
-  ssh_options[:keys] = "/Users/pramachandran/CBWorkspace/Projects/git/blogapp.pem"
-end
+
+
+ssh_options[:keys] = "/Users/pramachandran/CBWorkspace/Projects/git/blogapp.pem"
+
 
   
 # ssh_options[:keys] = "/home/ubuntu/.ssh/config"
